db3aac4d449d1e06827e397a59b152b33346d60b
Ben <benedict_sun@dlsu.edu.ph>
2022-06-19 09:44:20 +0800

    Workflow test [2]

--- getNetlifyFuncUsage.py
+++ getNetlifyFuncUsage.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
-
+    
 class NetlifyTracker:
     def __init__(self, email, password, sitenames, webhook_url):
         self.email = email
